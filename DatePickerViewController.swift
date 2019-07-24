aa1cda35ab21e65b0a19c6c2b1b6de1576a9e334
Marlon <marmoneymar@icloud.com>
2019-07-24 19:00:55 -0400

    Right there. It's just not passing the date correctly.

--- DatePickerViewController.swift
+++ DatePickerViewController.swift
@@ -8,20 +8,30 @@
 
 import UIKit
 
-#warning("Complete this function!")
-protocol DatePickerViewControllerDelegate {
-	func something(yep: String)
+protocol DatePickerDelegate: AnyObject {
+	func destinationDateWasChosen(date: Date)
 }
 
 class DatePickerViewController: UIViewController {
 
 	let layer = CAGradientLayer()
 
+	weak var delegate: DatePickerDelegate?
+
 	@IBOutlet weak var datePicker: UIDatePicker!
+	@IBOutlet weak var cancelButton: UIButton!
+	@IBOutlet weak var setDateButton: UIButton!
 
 
 	override func viewDidLoad() {
         super.viewDidLoad()
+
+		cancelButton.layer.borderWidth = 1
+		cancelButton.layer.borderColor = #colorLiteral(red: 0.479860858, green: 0.51219596, blue: 0.584455819, alpha: 1)
+		cancelButton.layer.cornerRadius = 8
+		setDateButton.layer.borderWidth = 1
+		setDateButton.layer.borderColor = #colorLiteral(red: 0.479860858, green: 0.51219596, blue: 0.584455819, alpha: 1)
+		setDateButton.layer.cornerRadius = 8
 
 		datePicker.setValue(UIColor(red: 0.31, green: 0.37, blue: 0.42, alpha: 1), forKey: "textColor")
 		
@@ -30,4 +40,16 @@ class DatePickerViewController: UIViewController {
 		view.layer.insertSublayer(layer, at: 0)
     }
 
+	@IBAction func cancelTapped(_ sender: UIButton) {
+		dismiss(animated: true, completion: nil)
+	}
+
+	@IBAction func setDateTapped(_ sender: UIButton) {
+		delegate?.destinationDateWasChosen(date: datePicker.date)
+		dismiss(animated: true, completion: nil)
+	}
+
+	
 }
+
+
--- TimeCircuitsViewController.swift
+++ TimeCircuitsViewController.swift
@@ -11,15 +11,70 @@ import UIKit
 class TimeCircuitsViewController: UIViewController {
 
 	let layer = CAGradientLayer()
+	var currentSpeed: Int = 0
+	var timer = Timer()
+	var datePickerVC = DatePickerViewController()
+
+	var dateFormatter: DateFormatter {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "MMM dd yyyy"
+		return formatter
+	}
+	@IBOutlet weak var destinationTimeLabel: UILabel!
+	@IBOutlet weak var presentTimeLabel: UILabel!
+	@IBOutlet weak var lastTimeDepartedLabel: UILabel!
+	@IBOutlet weak var speedLabel: UILabel!
+	@IBOutlet weak var travelBackButton: UIButton!
+	@IBOutlet weak var setDestinationTimeButton: UIButton!
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		travelBackButton.layer.borderWidth = 1
+		travelBackButton.layer.borderColor = #colorLiteral(red: 0.479860858, green: 0.51219596, blue: 0.584455819, alpha: 1)
+		travelBackButton.layer.cornerRadius = 8
+		setDestinationTimeButton.layer.borderWidth = 1
+		setDestinationTimeButton.layer.borderColor = #colorLiteral(red: 0.479860858, green: 0.51219596, blue: 0.584455819, alpha: 1)
+		setDestinationTimeButton.layer.cornerRadius = 8
+
 		layer.frame = view.bounds
 		layer.colors = [UIColor.black.cgColor, UIColor(red: 0.11, green: 0.13, blue: 0.13, alpha: 1).cgColor]
 		view.layer.insertSublayer(layer, at: 0)
+
+		presentTimeLabel.text = dateFormatter.string(from: Date())
+	}
+
+	func startTimer() {
+		timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(timer:))
+	}
+
+	func resetTimer() {
+		timer.invalidate()
+	}
+
+	func updateSpeed(timer: Timer) {
+		if currentSpeed < 88 {
+			currentSpeed += 1
+			speedLabel.text = "\(currentSpeed) MPH"
+		} else {
+			speedLabel.text = "\(currentSpeed) MPH"
+			currentSpeed = 0
+			lastTimeDepartedLabel.text = presentTimeLabel.text
+			presentTimeLabel.text = destinationTimeLabel.text
+			resetTimer()
+		}
+	}
+
+	@IBAction func travelBackButton(_ sender: UIButton) {
+		startTimer()
+	}
+}
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+	func destinationDateWasChosen(date: Date) {
+		destinationTimeLabel.text = dateFormatter.string(from: date)
 	}
 
 
 }
-
